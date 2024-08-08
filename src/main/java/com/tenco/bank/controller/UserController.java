4a3c2135fa53a8bf9c3d3791b778124509ac7672
dbstjlee <dbstjlee@naver.com>
2024-08-08 17:46:18 +0900

    이체 기능 구현

---
+++ src/main/java/com/tenco/bank/controller/UserController.java
@@ -0,0 +1,107 @@
+package com.tenco.bank.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tenco.bank.dto.SignInDTO;
+import com.tenco.bank.dto.SignUpDTO;
+import com.tenco.bank.handler.exception.DataDeliveryException;
+import com.tenco.bank.repository.model.User;
+import com.tenco.bank.service.UserService;
+import com.tenco.bank.utils.Define;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller  
+@RequestMapping("/user")  
+public class UserController {
+
+	private UserService userService;
+	private final HttpSession session;
+	
+	// DI 처리 
+	@Autowired // 노란색 경고는 사용할 필요 없음 - 가독성 위해서 선언해도 됨
+	public UserController(UserService service, HttpSession session) {
+		this.userService = service;
+		this.session = session;
+	}
+	
+	/**
+	 * 회원 가입 페이지 요청 
+	 * 주소 설계 : http://localhost:8080/user/sign-up
+	 * @return signUp.jsp 
+	 */
+	@GetMapping("/sign-up")
+	public String signUpPage() {
+		return "user/signUp";
+	}
+	
+	/**
+	 * 회원 가입 로직 처리 요청
+	 * 주소 설계 : http://localhost:8080/user/sign-up
+	 * @param dto
+	 * @return
+	 */
+	@PostMapping("/sign-up")
+	public String signUpProc(SignUpDTO dto) {
+		if(dto.getUsername() == null || dto.getUsername().isEmpty()) {
+			throw new DataDeliveryException(Define.ENTER_YOUR_USERNAME, HttpStatus.BAD_REQUEST);
+		}
+		
+		if(dto.getPassword() == null || dto.getPassword().isEmpty()) {
+			throw new DataDeliveryException(Define.ENTER_YOUR_PASSWORD, HttpStatus.BAD_REQUEST);
+		}
+		
+		if(dto.getFullname() == null || dto.getFullname().isEmpty()) {
+			throw new DataDeliveryException(Define.ENTER_YOUR_FULLNAME, HttpStatus.BAD_REQUEST);
+		}
+		userService.createUser(dto);
+		return "redirect:/user/sign-in";
+	}
+	
+	/**
+	 * 로그인 화면 요청 
+	 * @return
+	 */
+	@GetMapping("/sign-in")
+	public String singInPage() { 
+		return "user/signIn";
+	}
+	
+	
+	/**
+	 * 로그인 요청 처리 
+	 * 주소설계 : http://localhost:8080/user/sign-in
+	 * @return
+	 */
+	@PostMapping("/sign-in")
+	public String signProc(SignInDTO dto) {
+		if(dto.getUsername() == null || dto.getUsername().isEmpty()) {
+			throw new DataDeliveryException(Define.ENTER_YOUR_USERNAME, HttpStatus.BAD_REQUEST);
+		}
+		if(dto.getPassword() == null || dto.getPassword().isEmpty()) {
+			throw new DataDeliveryException(Define.ENTER_YOUR_PASSWORD, HttpStatus.BAD_REQUEST);
+		}
+		User principal = userService.readUser(dto);
+		
+		session.setAttribute(Define.PRINCIPAL, principal);
+		
+		return "redirect:/account/list"; 
+	}
+	
+	/**
+	 * 로그아웃 처리 
+	 * @return
+	 */
+	@GetMapping("/logout")
+	public String logout() {
+		session.invalidate(); 
+		return "redirect:/user/sign-in";
+	}
+	
+}
+
